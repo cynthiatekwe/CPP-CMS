e9f947d2d7ddd8a9ebee1fe4ac62b61da7c8e331
Delveri Munang <chickdelveri@gmail.com>
2015-08-20 20:37:53 +0100

    [CPPCMS-DEV] Virtual method for main application

--- main.cc
+++ main.cc
@@ -1,4 +1,23 @@
 #include <cppcms/application.h>
 #include <cppcms/applications_pool.h>
 #include <cppcms/service.h>
-#include <cppcms/http_request.h>
+#include <cppcms/http_response.h>
+#include <cppcms/url_dispatcher.h>
+#include <cppcms/url_mapper.h>
+
+
+class urlmain : public cppcms::application{
+public:
+	urlmain(cppcms::service &srv) :
+	cppcms::application(srv)
+	{
+
+	}
+
+	virtual void main(std::string ref);
+};
+
+void main(std::string ref){
+
+}
+
