20762f21f294185c32bc0096b41b0ee4a4829b92
Delveri Munang <chickdelveri@gmail.com>
2015-08-20 13:39:34 +0100

    Added constructor for various url patterns

--- url.cc
+++ url.cc
@@ -6,3 +6,35 @@
 #include <cppcms/url_mapper.h>
 #include <iostream>
 #include <stdlib.h>
+
+class urlex : public cppcms::application{
+public:
+	urlex(cppcms service &srv) : cppcms::application(srv){
+		dispatcher().assign("/number/(\\d+)", &urlex::number, this, 1);
+		mapper().assign("/number","/number/{1}");
+
+		dispatcher().assign("/smile",&urlex::smile,this);  
+    	mapper().assign("smile","/smile");  
+  
+	    dispatcher().assign("",&urlex::welcome,this);  
+	    mapper().assign("");
+
+
+	mapper().root("urlex");
+	}
+
+	void number(std::string num)  
+	{  
+	    int no = atoi(num.c_str());  
+	    response().out() << "The number is " << no << "<br/>\n";  
+	    response().out() << "<a href='" << url("/") << "'>Go back</a>";  
+	} 
+
+	void smile()  
+	{  
+	    response().out() << ":-) <br/>\n";  
+	    response().out() << "<a href='" << url("/") << "'>Go back</a>";  
+	}
+
+	virtual void main(std::string ref);
+};
