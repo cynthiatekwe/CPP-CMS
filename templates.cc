ee1c54bb5ba4d124c8cd01f0a3d5468b39de740d
Delveri Munang <chickdelveri@gmail.com>
2015-08-20 12:28:47 +0100

    Templating language example with cppcms

--- templates.cc
+++ templates.cc
@@ -8,3 +8,44 @@ bootstrap class styles.
     @Date   : August 2015
 */
 
+#include <cppcms/application.h>
+#include <cppcms/applications_pool.h>
+#include <cppcms/service.h>
+#include <cppcms/http_response.h>
+#include <iostream>
+
+class temp : public cppcms::application
+{
+public:
+    temp(cppcms::service &srv) : cppcms::application(srv)
+    {
+
+    }
+
+    virtual void main(std::string ref);
+
+};
+
+void temp::main(std::string ref)
+{
+    response().out() << "<html>\n"
+                        "<head>\n"
+                        "<title> template Tutorial </title>\n"
+                        "<body bgcolor='navy'>\n"
+                        "</body>\n"
+                        "</head>\n"
+                        "</html>\n";
+}
+
+int main(int argc,char ** argv)
+{
+    try{
+        cppcms::service srv(argc,argv);
+        srv.applications_pool().mount(
+            cppcms::applications_factory<temp>()
+            );
+        srv.run();
+    }catch(std::exception const &ex){
+std::cerr << ex.what() << std::endl;
+    }
+}
---
+++ tmpconf.js
@@ -0,0 +1,9 @@
+{  
+    "service" : {  
+        "api" : "http",  
+        "port" : 9001  
+    },  
+    "http" : {  
+        "script_names" : [ "/tem","/main" ]  
+    }  
+}
