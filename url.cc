c852b425dba3cc98c9ea1ff43ec7d9ec22cade05
Delveri Munang <chickdelveri@gmail.com>
2015-08-20 13:30:15 +0100

    Added url mapping /dispatching headers to application

---
+++ url.cc
@@ -0,0 +1,8 @@
+#include <cppcms/application.h>
+#include <cppcms/applications_pool.h>
+#include <cppcms/service.h>
+#include <cppcms/http_response.h>
+#include <cppcms/url_dispatcher.h>
+#include <cppcms/url_mapper.h>
+#include <iostream>
+#include <stdlib.h>
