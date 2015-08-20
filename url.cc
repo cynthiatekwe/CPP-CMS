d766d666b0987c6a389985ec7fcec18c364e76dd
Delveri Munang <chickdelveri@gmail.com>
2015-08-20 20:13:45 +0100

    [CPPCMS-DEV] Added number URL dispatcher and mapper

--- url.cc
+++ url.cc
@@ -10,10 +10,31 @@
 class urlex : public cppcms::application
 {
 public:
-	urlex(cppcms::service &srv) : cppcms::application(srv){
+	urlex(cppcms::service &srv) : 
+	cppcms::application(srv)
+	{
+		dispatcher().assign("/number/(\\d+)",&urlex::number, this, 1);
+		mapper().assign("/number/", "/number/{1}");
+
+		dispatcher().assign("/smile",&urlex::smile,this);  
+	    mapper().assign("smile","/smile");  
+	  
+	    dispatcher().assign("",&urlex::welcome,this);  
+	    mapper().assign("");  
+	  
+	    mapper().root("/urlex"); 
 
 	}
-	~urlex();
+
+			
+		void number(std::string num)  
+		{  
+		    int no = atoi(num.c_str());  
+		    response().out() << "The number is " << no << "<br/>\n";  
+		    response().out() << "<a href='" << url("/") << "'>Go back</a>";  
+		} 
+
+
 
 	virtual void main(std::string ref);
 	
