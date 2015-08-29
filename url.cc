4a1e794d01e9c8e6b965ae69ea148b63a512d310
Delveri Munang <chickdelveri@gmail.com>
2015-08-29 09:00:46 +0100

    [CPPCMS-DEV 4112]

--- url.cc
+++ url.cc
@@ -10,6 +10,18 @@
 class urlexample : cppcms::application{
 public:
 	urlexample(cppcms::service &srv):cppcms::application(srv){
-		
+		/*Assigning Urls to functions*/
+
+		dispatcher().assign("/number/(\\d+)",&urlexample::number,this,1);
+		mapper().assign("number","/number/{1}");
+
+		dispatcher().assign("/smile",&urlexample::smile, this);
+		mapper().assign("smile","/smile");
+
+		dispatcher().assign("",&urlexample::welcome,this);
+		mapper().assign("");
+
+		mapper().root("/urlexample");
+
 	}
 };
